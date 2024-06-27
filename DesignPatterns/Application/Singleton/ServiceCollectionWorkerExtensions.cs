5d9363372f237b5faff0d133ecdcf2b8fda815e4
kozlov.stepan <Stepan.Kozlov@infotecs.ru>
2024-06-27 18:04:52 +0300

    add singleton

---
+++ DesignPatterns/Application/Singleton/ServiceCollectionWorkerExtensions.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Singleton;
+
+
+/// <summary>
+/// Расширение для дообавления сервисов.
+/// </summary>
+public static class ServiceCollectionWorkerExtensions
+{
+    /// <summary>
+    /// Добавить работников.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/>.</param>
+    /// <returns></returns>
+    public static IServiceCollection AddWorkerSettings(this IServiceCollection services)
+    {
+        services.AddSingleton<IWorkerSettingsServiceDefault, WorkerSettingsServiceDefault>();
+        return services;
+    }
+}
---
+++ DesignPatterns/Application/Singleton/WorkerSettingsServiceDefault.cs
@@ -0,0 +1,26 @@
+using System.Reactive.Linq;
+
+namespace Application.Singleton;
+
+/// <summary>
+/// Сервис предоставления настроек работникам. С иницианализацией через DI.
+/// </summary>
+public class WorkerSettingsServiceDefault: IWorkerSettingsServiceDefault
+{
+    private WorkerSettingsServiceDefault() =>
+        SettingsObservable = Observable.Interval(TimeSpan.FromMinutes(5))
+            .StartWith(0)
+            .Select(x => ReadSettings());
+
+    /// <inheritdoc />
+    public IObservable<Dictionary<string, string>> SettingsObservable { get; }
+
+    /// <inheritdoc />
+    public Dictionary<string, string> GetCurrentSettings() => new Dictionary<string, string>();
+
+    private Dictionary<string, string> ReadSettings()
+    {
+        //read settings
+        return new Dictionary<string, string>();
+    }
+}
