82d62c8f6ff6fcd0e9ba8e604065b429d74f7634
MrSkap <58905153+MrSkap@users.noreply.github.com>
2024-07-20 15:15:00 +0300

    add composite pattern (#16)
    Co-authored-by: kozlov.stepan <2001skap@gmail.com>

---
+++ DesignPatterns/Application/Composite/FilterContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+
+namespace Application.Composite;
+
+/// <summary>
+/// Фильтр товаров.
+/// </summary>
+/// <param name="NameFilter">Название.</param>
+/// <param name="MaxWeight">Максимальный вес.</param>
+/// <param name="MinWeight">Минимальный  вес.</param>
+/// <param name="MaxSize">Максимальный размер.</param>
+/// <param name="MinSize">Минимальный размер.</param>
+/// <param name="Type">Тип.</param>
+public record FilterContext(string? NameFilter, float? MaxWeight, float? MinWeight, Size? MaxSize, Size? MinSize, GoodType? Type);
---
+++ DesignPatterns/Application/Composite/TypeFilter.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+
+namespace Application.Composite;
+
+/// <inheritdoc/>
+public class TypeFilter : Rule
+{
+    /// <inheritdoc/>
+    public TypeFilter(List<Rule>? rules = null)
+        : base(rules)
+    {
+        if (rules != null)
+        {
+            throw new Exception("This is leaf rule, it can not contain other rules");
+        }
+    }
+
+    /// <inheritdoc/>
+    public override List<Good> Filter(List<Good> goods, FilterContext context) =>
+        context.Type == null
+            ? goods
+            : goods.Where(x => x.Type == context.Type).ToList();
+}
